5c09937b5f3f6c2ff6e00d3bd43938cd8d9f6045
Konrad Słowik <konradslowik1998@gmail.com>
2021-04-11 16:53:43 +0200

    Add customer and create view

---
+++ MVCATP/Controllers/CountryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCATP.Models;
+
+namespace MVCATP.Controllers
+{
+    public class CountryController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CountryController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Country
+        public ActionResult Index()
+        {
+            var countries = _context.Countries.ToList();
+            return View(countries);
+        }
+    }
+}
---
+++ MVCATP/Controllers/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCATP.Models;
+
+namespace MVCATP.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public PlayerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Player
+        public ActionResult Index()
+        {
+            var players = _context.Players.Include(m => m.Country);
+            return View(players.ToList());
+        }
+    }
+}
